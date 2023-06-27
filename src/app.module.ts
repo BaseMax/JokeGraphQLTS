4731cdc19991ea6068627b57f79b17dd35f613df
Alireza Naghdipour <alirezanqp@gmail.com>
2023-06-27 12:26:52 +0330

    feat(app.module): import GraphQLModule and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { SharedModules } from './modules/shared/shared.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { JokeModule } from './modules/joke/joke.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    SharedModules,
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    JokeModule,
+    CategoryModule,
+  ],
 })
 export class AppModule {}
