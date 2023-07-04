5c4c5c1fce58994c0acb0d4b1e77341df9b27fdf
Alireza Naghdipour <alirezanqp@gmail.com>
2023-07-04 17:29:26 +0330

    update app port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
