52f43b1c1a0358eed572b3d833e06f579255eb97
Alireza Naghdipour <alirezanqp@gmail.com>
2023-07-04 10:22:12 +0330

    add upload module

---
+++ src/modules/upload/upload.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path from 'path';
+import { Public } from '../../common/decorators';
+
+@Controller('upload')
+export class UploadController {
+  @Public()
+  @Post('avatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const filename =
+            path.parse(file.originalname).name.replace(/\s/g, '') + Date.now();
+          const extension = path.parse(file.originalname).ext;
+          cb(null, `${filename}${extension}`);
+        },
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return { key: file.path };
+  }
+}
